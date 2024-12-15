b6c05545da79bf418454adcdac3c89c25b8cca54
dtata <dtata@DENIZ>
2024-12-15 19:41:20 +0300

    PaymentPage eklendi.

---
+++ MovieHall/Models/Item.cs
@@ -0,0 +1,25 @@
+﻿namespace MovieHall.Models
+{
+    public class Item
+    {
+        public int Id { get; set; } // Ürün ID'si
+        public string Name { get; set; } // Ürün adı
+        public decimal Price { get; set; } // Ürün fiyatı
+        public int Quantity { get; set; } // Ürün miktarı
+        public string Image { get; set; } // Ürün resim URL'si
+
+        // Toplam tutarı hesaplayan bir özellik
+        public decimal Total => Price * Quantity;
+
+
+    }
+
+    public class CartViewModel
+    {
+        public List<Item> Items { get; set; } // Sepetteki ürünler
+
+        // Toplam fiyatı hesaplayan bir özellik
+        public decimal TotalAmount => Items != null ? Items.Sum(item => item.Total) : 0;
+    }
+}
+
